43adbd6e0c18b0ff316e7da00440e93041ad6c53
Alexandr Gritsunov <ickpimo@gmail.com>
2023-09-01 22:09:08 +0300

    new module dicts.py

---
+++ utils/dicts.py
@@ -0,0 +1,11 @@
+def get_val(collection, key, default='git'):
+    """Функция возвращает значение из словаря по переданному ключу,
+    если ключ существует.В ином случае возвращается значение default"""
+
+    if len(collection) == 0:
+        return default
+
+    if key in collection:
+        return collection[key]
+    else:
+        return default
